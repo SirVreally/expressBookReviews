44e591ce8c71bf06ff5bfabe176b7fde230aaba5
Travis <SirVreally@outlook.com>
2026-02-12 17:34:01 -0500

    update to isbn GET

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,11 +31,18 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Retrieve the book parameter from the ISBN and show corresponding book
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn]);
- });
+
+  // Since books keys are numbers as strings, directly access the book by key
+  const book = books[isbn];
+
+  if (book) {
+    res.send(book);
+  } else {
+    res.status(404).send({ message: "Book not found for the given ISBN." });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
