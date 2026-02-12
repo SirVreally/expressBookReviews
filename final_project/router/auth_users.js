9c85435be725601f0a3eee45f07389e8a161ab7f
Travis <SirVreally@outlook.com>
2026-02-12 15:42:38 -0500

    updated auth_users.js with additional functionality with register user and book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -33,14 +33,45 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+      // Extract email parameter from request URL
+    const ISBN = req.params.isbn;
+    let isbn = isbn[isbn];  // Retrieve isbn object associated with review
+    if (isbn) {  // Check if friend exists
+        let review = req.body.review;
+        // Update review if provided in request body
+        if (review) {
+            isbn["Review"] = review;
+        }
+        isbn[review] = isbn;  // Update review in `isbn` object
+        res.send(`Book review ${review} has been added.`);
+    } else {
+        // Respond if isbn for book is not found to make review
+        res.send("Unable to add review!");
+    }
 });
 
 module.exports.authenticated = regd_users;
