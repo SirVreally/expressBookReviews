3b78ce7fcd4e2847e524f14b1cbe2822019dc20e
Travis <SirVreally@outlook.com>
2026-02-17 18:11:02 -0500

    test index change

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
+
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 const regd_users = require('./router/auth_users.js'); // adjust path as needed
