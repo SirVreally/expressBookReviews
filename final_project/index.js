3604bbab4bb98cb23e2eac85c182d6feacbd7300
Travis <SirVreally@outlook.com>
2026-02-17 19:43:20 -0500

    corrected to global app.use in index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}));
+app.use(session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}));
 
 app.use("/customer/auth/*", function auth(req,res,next){
     // Check if user is logged in and has valid access token
